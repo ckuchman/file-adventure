6287bb2af9686104a06a6c5374307380cc78e091
Christopher Kuchman <kuchmanc@os1.engr.oregonstate.edu>
2019-10-14 22:29:29 -0700

    Laid out starting text

---
+++ kuchmanc.buildrooms.c
@@ -0,0 +1,11 @@
+// TODO
+
+int main(void)
+{
+    //TODO Create a directory called kuchmanc.rooms.<PROCESS ID>
+    mkdir("./kuchmanc.rooms.TEST", 0700);
+
+    //TODO Generate 7 different room files
+
+    return 0; 
+}
