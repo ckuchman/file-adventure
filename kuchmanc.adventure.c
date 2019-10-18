91931202f41aadc149530020c9f143739d93f0b9
Christopher Kuchman <kuchmanc@os1.engr.oregonstate.edu>
2019-10-17 21:17:24 -0700

    found newest rooms folder

--- kuchmanc.adventure.c
+++ kuchmanc.adventure.c
@@ -1,30 +1,49 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <time.h>
 
 int main() {
 
     //Find the most recent rooms folder
-    char *roomDir[50];
+    char roomDir[50];
     memset(roomDir, '\0', 50*sizeof(char));
 
     //Open current directory
     //Code source taken from https://c-for-dummies.com/blog/?p=3246
     DIR *directory;
     struct dirent *entry;
+
     directory = opendir(".");
+
+    struct stat statBuffer;
+    int recentTime = -1; 
 
     //Look through directory to find newest room folder
     while (entry = readdir(directory)){
-        printf("File: %s\n", entry->d_name);
+        stat(entry->d_name, &statBuffer);
+
+        if ((int)statBuffer.st_mtime > recentTime && \
+            strstr(entry->d_name, "kuchmanc.rooms") != NULL) {
+            recentTime = (int)statBuffer.st_mtime;
+            sprintf(roomDir, entry->d_name); 
+        }
     }
+
+    printf("Final Dir: %s", roomDir);
 
     //Close current directory
     closedir(directory);
 
+    //Open rooms directory
+    DIR *roomDirectory;
 
-    //Find the starting room
+    roomDirectory = opendir(roomDir);
+
     
+    closedir(roomDirectory);
 
     //Game loop while not at end room
 
